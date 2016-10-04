bf4be35926550a03e50fb03af94aa4168f0e1aca
Evgeniy Generalov <evgeniy_generalov@epam.com>
2016-10-04 19:10:40 +0300

    fix eslint violations

--- karma.conf.js
+++ karma.conf.js
@@ -1,4 +1,4 @@
-/* eslint node: true */
+/* eslint-env node */
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/0.13/config/configuration-file.html
 
--- src/webpack-bundle.config.js
+++ src/webpack-bundle.config.js
@@ -1,4 +1,4 @@
-/* eslint node: true */
+/* eslint-env node */
 const path = require('path');
 const webpack = require('webpack');
 
