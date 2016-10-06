a2d7093465c11470d6df40417ad98d3e7cffc83e
Evgeniy Generalov <evgeniy_generalov@epam.com>
2016-10-06 18:28:38 +0300

    fix tests

--- src/angular-http-mock/src/http-mock.module.spec.ts
+++ src/angular-http-mock/src/http-mock.module.spec.ts
@@ -1,10 +1,10 @@
 import {Response, ResponseOptions, HttpModule, Http} from '@angular/http';
 import {inject, TestBed} from '@angular/core/testing';
+import {MockBackend} from '@angular/http/testing';
 
 import {HttpMock} from './http-mock';
 import {HttpMockModule} from './http-mock.module';
-import {MockBackend} from '@angular/http/testing';
-import createSpy = jasmine.createSpy;
+import {HttpMockError} from './http-mock-error';
 
 
 describe('HttpMockModule', () => {
@@ -21,24 +21,47 @@ describe('HttpMockModule', () => {
     expect(mock).toBeDefined();
   }));
 
-  it('should mock GET request', (inject([HttpMock, Http], (mock: HttpMock, http: Http) => {
-    const responseData: ResponseOptions = new ResponseOptions({
+  it('should mock response', inject([HttpMock, Http], (mock: HttpMock, http: Http) => {
+    const nextFn = jasmine.createSpy('next');
+    const errorFn = jasmine.createSpy('error');
+    const options = {
       status: 200,
       body: 'ok'
-    });
-    const nextFn = createSpy('next', (response: Response) => {
-      expect(response.url).toEqual('http://testserver/api/');
-      expect(response.status).toEqual(responseData.status);
-      expect(response.text()).toEqual(responseData.body);
-    });
-    const errorFn = createSpy('error', (error: any) => {});
-    mock.match({url: 'http://testserver/api/'}).andRespond(responseData);
+    };
+    mock.match({url: 'http://testserver/api/'}).andRespond(new ResponseOptions(options));
 
-    http.get('http://testserver/api/').subscribe(nextFn, errorFn);
+    http.get('http://testserver/api/').subscribe(
+      nextFn.and.callFake((response: Response) => {
+        expect(response.status).toEqual(200);
+        expect(response.url).toEqual('http://testserver/api/');
+        expect(response.text()).toEqual('ok');
+      }),
+      errorFn
+    );
 
     expect(nextFn).toHaveBeenCalled();
     expect(errorFn).not.toHaveBeenCalled();
-  })));
+  }));
+
+  it('should mock error response', inject([HttpMock, Http], (mock: HttpMock, http: Http) => {
+    const nextFn = jasmine.createSpy('next');
+    const errorFn = jasmine.createSpy('error');
+    const response = {
+      status: 400,
+      body: 'Bad request'
+    };
+    mock.match({url: 'http://testserver/api/'}).andRespond(new ResponseOptions(response));
+
+    http.get('http://testserver/api/').subscribe(
+      nextFn,
+      errorFn.and.callFake((error: HttpMockError) => {
+        expect(error.status).toEqual(400);
+        expect(error.message).toEqual('Bad request');
+      })
+    );
+
+    expect(nextFn).not.toHaveBeenCalled();
+    expect(errorFn).toHaveBeenCalled();
+  }));
 
 });
-
